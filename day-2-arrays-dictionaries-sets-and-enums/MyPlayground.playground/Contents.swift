c278c78047e7266282c56dbb9bb7e69a584845fb
Swapnil Godambe <swapnil.go20@gmail.com>
2020-10-22 08:31:33 +0530

    Arrays

---
+++ day-2-arrays-dictionaries-sets-and-enums/MyPlayground.playground/Contents.swift
@@ -0,0 +1,11 @@
+import UIKit
+
+var str = "Hacking with Swift - Day 2"
+
+let john = "John Lennon"
+let paul = "Paul McCartney"
+let george = "George Harrison"
+let ringo = "Ringo Starr"
+
+let beatles = [john, paul, george, ringo]
+beatles[1]
