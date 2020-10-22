16b5ef185391c343e00232e6c051ee0294cfa274
Swapnil Godambe <swapnil.go20@gmail.com>
2020-10-22 08:49:28 +0530

    Tuples

--- day-2-arrays-dictionaries-sets-and-enums/MyPlayground.playground/Contents.swift
+++ day-2-arrays-dictionaries-sets-and-enums/MyPlayground.playground/Contents.swift
@@ -9,3 +9,15 @@ let ringo = "Ringo Starr"
 
 let beatles = [john, paul, george, ringo]
 beatles[1]
+
+let colors = Set(["red", "green", "blue"])
+let colors2 = Set(["red", "green", "blue", "red", "blue"])
+
+
+var name = (first: "Swapnil", last: "Godambe")
+name.0
+name.first
+//name.age = 30
+
+
+
