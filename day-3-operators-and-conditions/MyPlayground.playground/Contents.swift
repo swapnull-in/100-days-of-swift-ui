6e78fb552af1765749e78688778bdb4e63de4e26
Swapnil Godambe <swapnil.go20@gmail.com>
2020-10-23 16:10:42 +0530

    Combining conditions

--- day-3-operators-and-conditions/MyPlayground.playground/Contents.swift
+++ day-3-operators-and-conditions/MyPlayground.playground/Contents.swift
@@ -65,3 +65,18 @@ else if firstCard + secondCard == 21 {
     print("Regular cards")
 }
 
+//Combining conditions
+
+let age1 = 12
+let age2 = 21
+
+if age1 > 18 && age2 > 18 {
+    print("Both are over 18")
+}
+
+if age1 > 18 || age2 > 18 {
+    print("At least one is over 18")
+}
+
+
+
