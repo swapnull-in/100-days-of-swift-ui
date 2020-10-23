500e2b950277425c79d59c71ff3d401961247b7e
Swapnil Godambe <swapnil.go20@gmail.com>
2020-10-23 17:36:22 +0530

    Range operators

--- day-3-operators-and-conditions/MyPlayground.playground/Contents.swift
+++ day-3-operators-and-conditions/MyPlayground.playground/Contents.swift
@@ -101,3 +101,17 @@ default:
     print("Enjoy your day!")
 }
 
+//Range operators
+
+let score1 = 85
+
+switch score1 {
+case 0..<50:
+    print("You failed badly.")
+case 50..<85:
+    print("You did OK.")
+default:
+    print("You did great!")
+}
+
+
