91046fae7af0cafe40eb67bb0937a7b23c93cf4b
Swapnil Godambe <swapnil.go20@gmail.com>
2020-10-24 13:40:26 +0530

    Exiting loops

--- day-4-loops-loops-and-more-loops/MyPlayground.playground/Contents.swift
+++ day-4-loops-loops-and-more-loops/MyPlayground.playground/Contents.swift
@@ -54,4 +54,22 @@ repeat {
 } while false
 
 
+//Exiting loops
+
+var countDown = 10
+
+while countDown >= 0 {
+    print(countDown)
+    
+    if countDown == 4 {
+        print("I'm bored. Let's go now!")
+        break
+    }
+    
+    countDown -= 1
+}
+
+print("Blast off!")
+
+
 
