e9d0c0a9be62703e1fac059f1a50a514e5016955
Swapnil Godambe <swapnil.go20@gmail.com>
2020-10-25 14:56:18 +0530

    Default parameters

--- day-5–functions-parameters-and-errors/MyPlayground.playground/Contents.swift
+++ day-5–functions-parameters-and-errors/MyPlayground.playground/Contents.swift
@@ -52,5 +52,17 @@ func greet(_ person: String) {
 greet("Tylor")
 
 
+//Default parameters
+
+func greet(_ person: String, nicely: Bool = true) {
+    if nicely == true {
+        print("Hello \(person)")
+    }else {
+        print("Oh no, it's \(person) again...")
+    }
+}
+
+greet("Taylor")
+greet("Taylor", nicely: false)
 
 
