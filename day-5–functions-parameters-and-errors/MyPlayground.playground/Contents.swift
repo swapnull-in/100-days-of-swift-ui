7038924f82be668dc9f061a69ab0d459ca532ce0
Swapnil Godambe <swapnil.go20@gmail.com>
2020-10-25 10:27:58 +0530

    Writing functions

---
+++ day-5–functions-parameters-and-errors/MyPlayground.playground/Contents.swift
@@ -0,0 +1,19 @@
+import UIKit
+
+var str = "Day 5 – functions, parameters, and errors"
+
+//Writing functions
+
+func printHelp() {
+    
+    let message = """
+       Welcome to MyApp!
+
+       Run this app inside a directory of images and
+       MyApp will resize them all into thumbnails
+    """
+    
+    print(message)
+}
+
+printHelp()
