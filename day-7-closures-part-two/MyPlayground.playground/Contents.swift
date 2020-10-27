59f3d620089f4723d25dc587a6756c98cb6de497
Swapnil Godambe <swapnil.go20@gmail.com>
2020-10-27 07:43:22 +0530

    Shorthand parameter names

--- day-7-closures-part-two/MyPlayground.playground/Contents.swift
+++ day-7-closures-part-two/MyPlayground.playground/Contents.swift
@@ -27,5 +27,16 @@ travel1 { (place : String) -> String in
     return "I'm going to \(place) in my car"
 }
 
+//Shorthand parameter names
 
+func travel2(action: (String) -> String) {
+    print("I'm getting ready to go.")
+    let description = action("London")
+    print(description)
+    print("I arrived!")
+}
+
+travel2 {
+     "I'm going to \($0) in my car"
+}
 
