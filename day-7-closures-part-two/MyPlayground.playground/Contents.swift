d4b1277becc93ec1b62300279273b552436799c1
Swapnil Godambe <swapnil.go20@gmail.com>
2020-10-27 07:31:01 +0530

    Using closures as parameters when they return values

--- day-7-closures-part-two/MyPlayground.playground/Contents.swift
+++ day-7-closures-part-two/MyPlayground.playground/Contents.swift
@@ -14,3 +14,18 @@ travel { (place: String) in
     print("I'm going to \(place) in my car")
 }
 
+//Using closures as parameters when they return values
+
+func travel1(action: (String) -> String) {
+    print("I'm getting ready to go.")
+    let description = action("London")
+    print(description)
+    print("I arrived!")
+}
+
+travel1 { (place : String) -> String in
+    return "I'm going to \(place) in my car"
+}
+
+
+
