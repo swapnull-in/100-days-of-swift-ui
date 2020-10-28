483c0b6d1000dac9b3ae2e37272366a1df6390c5
Swapnil Godambe <swapnil.go20@gmail.com>
2020-10-28 08:50:40 +0530

    Methods

--- day-8–structs-properties-and-methods/MyPlayground.playground/Contents.swift
+++ day-8–structs-properties-and-methods/MyPlayground.playground/Contents.swift
@@ -47,3 +47,19 @@ var progress = Progress(task: "Loading data", amount: 0)
 progress.amount = 30
 progress.amount = 80
 progress.amount = 100
+
+//Methods
+
+struct City {
+    var population: Int
+    
+    func collectTaxes() -> Int {
+        return population * 1000
+    }
+}
+
+var london = City(population: 9_000_000)
+london.collectTaxes()
+
+
+
