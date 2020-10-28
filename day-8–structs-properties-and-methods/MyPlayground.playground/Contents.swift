4828f25169311aada139ccc3100e2553c73875d0
Swapnil Godambe <swapnil.go20@gmail.com>
2020-10-28 08:43:11 +0530

    Computed properties

--- day-8–structs-properties-and-methods/MyPlayground.playground/Contents.swift
+++ day-8–structs-properties-and-methods/MyPlayground.playground/Contents.swift
@@ -4,15 +4,31 @@ var str = "Day 8 – structs, properties, and methods"
 
 //Creating your own structs
 
+//struct Sport {
+//    var name: String
+//}
+//
+//var tennis = Sport(name: "Tennis")
+//print(tennis.name)
+//
+//tennis.name = "Lawn Tennis"
+
+
+//Computed properties
+
 struct Sport {
     var name: String
+    var isOlympicSport: Bool
+    
+    var olympicStatus: String {
+        if isOlympicSport {
+            return "\(name) is an Olympic sport"
+        }else {
+            return "\(name) is not an Olympic sport"
+        }
+    }
 }
 
-var tennis = Sport(name: "Tennis")
-print(tennis.name)
-
-tennis.name = "Lawn Tennis"
-
-
-
+var chessBoxing = Sport(name: "Chessboxing", isOlympicSport: false)
+print(chessBoxing.olympicStatus)
 
