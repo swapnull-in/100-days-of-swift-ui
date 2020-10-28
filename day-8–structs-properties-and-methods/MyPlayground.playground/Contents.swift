0dad1dea4ef33ae1ca5bc791d4ac9232e6ba1ace
Swapnil Godambe <swapnil.go20@gmail.com>
2020-10-28 08:37:30 +0530

    Creating your own structs

---
+++ day-8–structs-properties-and-methods/MyPlayground.playground/Contents.swift
@@ -0,0 +1,18 @@
+import UIKit
+
+var str = "Day 8 – structs, properties, and methods"
+
+//Creating your own structs
+
+struct Sport {
+    var name: String
+}
+
+var tennis = Sport(name: "Tennis")
+print(tennis.name)
+
+tennis.name = "Lawn Tennis"
+
+
+
+
