6a194b466d50aa7227b15802c1eecd89a1d0d6a4
Swapnil Godambe <swapnil.go20@gmail.com>
2020-10-28 09:25:02 +0530

    Properties and methods of strings

--- day-8–structs-properties-and-methods/MyPlayground.playground/Contents.swift
+++ day-8–structs-properties-and-methods/MyPlayground.playground/Contents.swift
@@ -74,4 +74,12 @@ struct Person {
 var person = Person(name: "Ed")
 person.makeAnonymous()
 
+//Properties and methods of strings
+
+let string = "Do or do not, there is no try."
+print(string.count)
+print(string.hasPrefix("Do"))
+print(string.uppercased())
+print(string.sorted())
+
 
