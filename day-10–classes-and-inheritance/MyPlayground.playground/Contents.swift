3df0df6372d135340a06e6db4259e87fd1148e1c
Swapnil Godambe <swapnil.go20@gmail.com>
2020-10-30 09:38:39 +0530

    Deinitializers

--- day-10–classes-and-inheritance/MyPlayground.playground/Contents.swift
+++ day-10–classes-and-inheritance/MyPlayground.playground/Contents.swift
@@ -98,7 +98,29 @@ singerCopy.name = "Katy Perry"
 
 print(singer.name)
 
+//Deinitializers
 
+class Person {
+    var name = "John Doe"
+    
+    init() {
+        print("\(name) is alive!")
+    }
+    
+    func printGreetings(){
+        print("Hello, I'm \(name)")
+    }
+    
+    deinit {
+        print("\(name) is no more!")
+    }
+    
+}
+
+for _ in 1...3 {
+    let person = Person()
+    print(person.name)
+}
 
 
 
