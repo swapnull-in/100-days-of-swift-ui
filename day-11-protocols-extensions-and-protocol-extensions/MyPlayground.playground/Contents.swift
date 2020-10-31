b3aabacfec123461c76e8a23d8f0ac48f4cc0d11
Swapnil Godambe <swapnil.go20@gmail.com>
2020-10-31 09:29:53 +0530

    Protocol extensions

--- day-11-protocols-extensions-and-protocol-extensions/MyPlayground.playground/Contents.swift
+++ day-11-protocols-extensions-and-protocol-extensions/MyPlayground.playground/Contents.swift
@@ -49,3 +49,22 @@ extension Int {
     }
 }
 
+//Protocol extensions
+
+let pythons = ["Eric", "Graham", "John", "Michael", "Terry", "Terry"]
+let beatles = Set(["John", "Paul", "George", "Ringo"])
+
+extension Collection {
+    func summarize () {
+        print("There are \(count) of us")
+        
+        for name in self {
+            print(name)
+        }
+    }
+}
+
+pythons.summarize()
+beatles.summarize()
+
+
