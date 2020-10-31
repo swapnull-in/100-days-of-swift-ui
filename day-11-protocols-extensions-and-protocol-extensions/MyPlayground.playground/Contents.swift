47f4233469be46e4da08f001b10411c5c93f8563
Swapnil Godambe <swapnil.go20@gmail.com>
2020-10-31 09:22:13 +0530

    Protocol inheritance

--- day-11-protocols-extensions-and-protocol-extensions/MyPlayground.playground/Contents.swift
+++ day-11-protocols-extensions-and-protocol-extensions/MyPlayground.playground/Contents.swift
@@ -16,3 +16,20 @@ func displayID(thing: Identifiable){
     print("My ID is \(thing.id)")
 }
 
+//Protocol inheritance
+
+protocol Payable {
+    func calculateWages() -> Int
+}
+
+protocol NeedsTraining {
+    func study()
+}
+
+protocol HasVacation {
+    func takeVacation(days: Int)
+}
+
+protocol Employee : Payable, NeedsTraining, HasVacation {}
+
+
