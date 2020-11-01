53765e13577fb9161828c25e5a5c3fe825faea71
Swapnil Godambe <swapnil.go20@gmail.com>
2020-11-01 10:59:13 +0530

    Optional try

--- day-12-optionals-unwrapping-and-typecasting/MyPlayground.playground/Contents.swift
+++ day-12-optionals-unwrapping-and-typecasting/MyPlayground.playground/Contents.swift
@@ -59,6 +59,35 @@ let names = ["John", "Paul", "George", "Ringo"]
 
 let beatle = names.first?.uppercased()
 
+//Optional try
+
+enum PasswordError: Error {
+    case obvious
+}
+
+func checkPassword(_ password: String) throws -> Bool {
+    if password == "password" {
+        throw PasswordError.obvious
+    }
+
+    return true
+}
+
+do {
+    try checkPassword("password")
+    print("That password is good!")
+} catch {
+    print("You can't use that password.")
+}
+
+if let result = try? checkPassword("password") {
+    print("Result was \(result)")
+}else {
+    print("D'oh.")
+}
+
+try! checkPassword("swapnull")
+print("OK!")
 
 
 
