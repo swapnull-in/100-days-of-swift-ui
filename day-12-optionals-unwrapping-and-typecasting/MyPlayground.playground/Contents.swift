4262ef16bbbca846a864d6f82cb41f3a57501852
Swapnil Godambe <swapnil.go20@gmail.com>
2020-11-01 12:11:13 +0530

    Typecasting

--- day-12-optionals-unwrapping-and-typecasting/MyPlayground.playground/Contents.swift
+++ day-12-optionals-unwrapping-and-typecasting/MyPlayground.playground/Contents.swift
@@ -106,4 +106,23 @@ struct Person {
     }
 }
 
+//Typecasting
+
+class Animal {}
+class Fish: Animal {}
+
+class Dog: Animal {
+    func makeNoise() {
+        print("Woof!")
+    }
+}
+
+let pets = [Fish(), Dog(), Fish(), Dog()]
+
+for pet in pets {
+    if let dog = pet as? Dog {
+        dog.makeNoise()
+    }
+}
+
 
