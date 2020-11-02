649a4b175bc9b95a99a17e30ca644cb18da189c4
Swapnil Godambe <swapnil.go20@gmail.com>
2020-11-02 08:28:25 +0530

    Variables and constants

---
+++ day-13-swift-review-day-one/MyPlayground.playground/Contents.swift
@@ -0,0 +1,12 @@
+import UIKit
+
+var str = "Day 13 – Swift review, day one"
+
+//Variables and constants
+
+var name = "Tim McGraw"
+    name = "Romeo"
+
+//let name = "Tim McGraw"
+//    name = "Romeo"
+
