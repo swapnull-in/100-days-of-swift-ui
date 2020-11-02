e52ec9b0b3437c804e769b48809222b33b0c6fdd
Swapnil Godambe <swapnil.go20@gmail.com>
2020-11-03 00:38:29 +0530

    Loops

--- day-13-swift-review-day-one/MyPlayground.playground/Contents.swift
+++ day-13-swift-review-day-one/MyPlayground.playground/Contents.swift
@@ -132,3 +132,67 @@ if stayOutTooLate2 && nothingInBrain2 {
     action2 = "cruise"
 }
 
+//Loops
+
+for i in 1...10 {
+    print("\(i) x 10 is \(i * 10)")
+}
+
+var str1 = "Fakers gonna"
+
+for _ in 1 ... 5 {
+    str1 += " fake"
+}
+
+print(str1)
+
+
+var songs4 = ["Shake it Off", "You Belong with Me", "Look What You Made Me Do"]
+
+for song in songs4 {
+    if song == "You Belong with Me" {
+        continue
+    }
+
+    print("My favorite song is \(song)")
+}
+
+var people = ["players", "haters", "heart-breakers", "fakers"]
+var actions = ["play", "hate", "break", "fake"]
+
+for i in 0 ..< people.count {
+    print("\(people[i]) gonna \(actions[i])")
+}
+
+for i in 0 ..< people.count {
+    var str = "\(people[i]) gonna"
+
+    for _ in 1 ... 5 {
+        str += " \(actions[i])"
+    }
+
+    print(str)
+}
+
+var counter = 0
+
+while true {
+    print("Counter is now \(counter)")
+    
+    counter += 1
+    
+    if counter == 51 {
+        break
+    }
+}
+
+var songs5 = ["Shake it Off", "You Belong with Me", "Look What You Made Me Do"]
+
+for song in songs5 {
+    if song == "You Belong with Me" {
+        continue
+    }
+
+    print("My favorite song is \(song)")
+}
+
