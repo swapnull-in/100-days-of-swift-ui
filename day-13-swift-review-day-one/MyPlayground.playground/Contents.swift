cae53bcab7cabdf4855fd13473d56e16a9e758ab
Swapnil Godambe <swapnil.go20@gmail.com>
2020-11-03 00:38:46 +0530

    Switch

--- day-13-swift-review-day-one/MyPlayground.playground/Contents.swift
+++ day-13-swift-review-day-one/MyPlayground.playground/Contents.swift
@@ -196,3 +196,39 @@ for song in songs5 {
     print("My favorite song is \(song)")
 }
 
+//Switch case
+
+let liveAlbums = 2
+
+switch liveAlbums {
+case 0:
+    print("You're just starting out")
+
+case 1:
+    print("You just released iTunes Live From SoHo")
+
+case 2:
+    print("You just released Speak Now World Tour")
+
+default:
+    print("Have you done something new?")
+}
+
+
+let studioAlbums = 5
+
+switch studioAlbums {
+case 0...1:
+    print("You're just starting out")
+
+case 2...3:
+    print("You're a rising star")
+
+case 4...5:
+    print("You're world famous!")
+
+default:
+    print("Have you done something new?")
+}
+
+
