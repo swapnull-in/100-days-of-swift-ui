7a25fdc2ac1212c6223cae8898367e76fd95b598
Swapnil Godambe <swapnil.go20@gmail.com>
2020-11-02 09:03:07 +0530

    Operators

--- day-13-swift-review-day-one/MyPlayground.playground/Contents.swift
+++ day-13-swift-review-day-one/MyPlayground.playground/Contents.swift
@@ -24,13 +24,45 @@ latitude = 36.166667
 var longitude: Double
 longitude = -123456786.783333
 
-var stayOutToLate: Bool
-stayOutToLate = true
+var stayOutTooLate: Bool
+stayOutTooLate = true
 
 var nothingInBrain: Bool
 nothingInBrain = true
 
 var missABeat = false
+
+//Operators
+
+var a = 10
+a = a + 1
+a = a - 1
+a = a * a
+
+var b = 10
+b += 10
+b -= 10
+
+var a1 = 1.1
+var b1 = 2.2
+var c1 = a1 + b1
+
+var name1 = "Tim McGraw"
+var name2 = "Romeo"
+var both = name1 + " and " + name2
+
+c1 > 3
+c1 >= 3
+c1 > 4
+c1 < 4
+
+var name3 = "Tim McGraw"
+name3 == "TIM McGraw"
+name3 != "TIM McGraw"
+
+var stayOutTooLate1 = true
+stayOutTooLate1
+!stayOutTooLate1
 
 
 
