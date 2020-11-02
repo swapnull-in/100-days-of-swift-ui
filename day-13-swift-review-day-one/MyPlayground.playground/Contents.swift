6a616c8eaa2181e0157eaf45f67b8d29629a7bbc
Swapnil Godambe <swapnil.go20@gmail.com>
2020-11-02 08:52:38 +0530

    Types of Data

--- day-13-swift-review-day-one/MyPlayground.playground/Contents.swift
+++ day-13-swift-review-day-one/MyPlayground.playground/Contents.swift
@@ -4,9 +4,33 @@ var str = "Day 13 – Swift review, day one"
 
 //Variables and constants
 
-var name = "Tim McGraw"
-    name = "Romeo"
+//var name = "Tim McGraw"
+//    name = "Romeo"
 
 //let name = "Tim McGraw"
 //    name = "Romeo"
+
+//Types of Data
+
+var name: String
+    name = "Tim McGraw"
+
+var age: Int
+    age = 25
+
+var latitude: Double
+latitude = 36.166667
+
+var longitude: Double
+longitude = -123456786.783333
+
+var stayOutToLate: Bool
+stayOutToLate = true
+
+var nothingInBrain: Bool
+nothingInBrain = true
+
+var missABeat = false
+
+
 
