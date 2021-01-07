2b2bf1a248214a007431e038ee4d60ee7eb99afe
Swapnil Godambe <swapnil.go20@gmail.com>
2021-01-07 10:34:56 +0530

    Day 25

---
+++ day-25-milestone-projects-1-3/Brain Training/Brain Training/Brain_TrainingApp.swift
@@ -0,0 +1,17 @@
+//
+//  Brain_TrainingApp.swift
+//  Brain Training
+//
+//  Created by Swapnil Godambe on 07/01/21.
+//
+
+import SwiftUI
+
+@main
+struct Brain_TrainingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
