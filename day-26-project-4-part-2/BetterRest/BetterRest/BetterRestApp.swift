a25882eb2761601491a4b750d1d8845466586b62
Swapnil Godambe <swapnil.go20@gmail.com>
2021-01-10 09:04:27 +0530

    Day 26

---
+++ day-26-project-4-part-2/BetterRest/BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Swapnil Godambe on 10/01/21.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
