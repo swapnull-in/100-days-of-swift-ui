a81fe22605df10829f31022493f17cc6ee2152b3
Swapnil Godambe <swapnil.go20@gmail.com>
2021-01-30 19:44:24 +0530

    Day 29

---
+++ day-29-project-5-part-1/WordScramble/WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Swapnil Godambe on 30/01/21.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
