9c9650cebec7f89067a54d359a9cfefc77fe9030
Swapnil Godambe <swapnil.go20@gmail.com>
2021-01-31 16:59:16 +0530

    Day 32

---
+++ day-32-project-6-part-1/Animations/Animations/AnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationsApp.swift
+//  Animations
+//
+//  Created by Swapnil Godambe on 31/01/21.
+//
+
+import SwiftUI
+
+@main
+struct AnimationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
